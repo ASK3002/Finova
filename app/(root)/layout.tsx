919f6b0fd05e302f77687d716f5287ea68571c77
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2026-01-14 01:53:06 +0530

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+export const dynamic = 'force-dynamic'
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
